54dac5833fcaf31f832336b726aac40fadd5755f
atang35 <amahloma@emich.edu>
2024-08-20 11:58:51 +0200

    Added Git Project

--- scripts.r
+++ scripts.r
@@ -3,7 +3,7 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Atang Mahlomaholo",
   data_location = "https://docs.google.com/spreadsheets/d/1giJe9kPDmodxkWV-ueerneTHiHGgHzxER71D6SM53uU/edit?pli=1&gid=0#gid=0",
-  pdf_location = "",
+  pdf_location = "https://github.com/atang35/Atang-Resume/blob/main/cv.pdf",
   pdf_location_resume = "https://github.com/atang35/Atang-Resume/blob/main/cv.pdf",
   html_location = "https://atang35.github.io/Atang-Resume/",
   html_location_resume = "https://atang35.github.io/Atang-Resume/",
