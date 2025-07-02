66d3e889f939949a852e9868d731e823e6da853f
atang35 <amahloma@emich.edu>
2025-07-02 16:24:30 +0200

    2025 upadated

--- scripts.r
+++ scripts.r
@@ -2,10 +2,11 @@
 
 datadrivencv::use_datadriven_cv(
   full_name = "Atang Mahlomaholo",
-  data_location = "https://docs.google.com/spreadsheets/d/1giJe9kPDmodxkWV-ueerneTHiHGgHzxER71D6SM53uU/edit?pli=1&gid=0#gid=0",
+  data_location = "https://docs.google.com/spreadsheets/d/1eh8Yjwp-Uw6k3vkR5IsSS6c2Mk-esiIz7L-0sQ5hNCE/edit?gid=1730172225#gid=1730172225",  #system.file("data/", package = "datadrivencv"),
   pdf_location = "https://github.com/atang35/Atang-Resume/blob/main/cv.pdf",
   pdf_location_resume = "https://github.com/atang35/Atang-Resume/blob/main/cv.pdf",
   html_location = "https://atang35.github.io/Atang-Resume/",
   html_location_resume = "https://atang35.github.io/Atang-Resume/",
   source_location = "https://github.com/atang35/Atang-Resume/blob/main/cv.pdf"
 )
+
